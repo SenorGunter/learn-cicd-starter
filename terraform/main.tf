db979699e8d0942a78b890869c48726733cad245
SenorGunter <163559186+SenorGunter@users.noreply.github.com>
2025-09-18 20:57:31 +0200

    adding Terraform for automated EC2 instances

---
+++ terraform/main.tf
@@ -0,0 +1,74 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# Security Groups
+resource "aws_security_group" "notely_sg" {
+  name = "notely_sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    description = "HTTP"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# Key pair public key
+resource "aws_instance" "notely_key" {
+  key_name = var.ec2_key_name
+  public_key = var.ec2_public_key
+}
+
+
+# EC2 instance
+resource "aws_instance" "notely_ec2" {
+  ami = var.ami_id
+  instance_type = var.ec2_instance_type
+  key_name = aws_key_pair.notely_key.key_name
+  vpc_security_group_ids = [aws_security_group.notely_sg]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install docker -y
+              service docker start
+              usermod -a -G docker ec2-user
+              aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com
+              docker pull ${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${var.ecr_repository}:latest
+              docker run -d --restart unless-stopped -p 80:80 ${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${var.ecr_repository}:latest
+              EOF
+
+  tags = {
+    Name = "NotelyApp"
+}
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+
+
+
+variable "aws_region" {}
+variable "aws_account_id" {}
+variable "ecr_repository" {}
+variable "ec2_instance_type" {
+  default = "t3.micro"
+}
+variable "ami_id" {}
+variable "ec2_public_key" {}
+variable "ec2_key_name" {}
