158cd3ddeb5afbebaa049a8f2ad64a52ff86cd09
SenorGunter <163559186+SenorGunter@users.noreply.github.com>
2025-09-19 16:27:13 +0200

    trying fix6

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,6 @@ resource "aws_instance" "notely_ec2" {
   instance_type = var.ec2_instance_type
   key_name = var.ec2_key_name
   vpc_security_group_ids = [var.security_group_id]
-
   associate_public_ip_address = true
 
   user_data = <<-EOF
@@ -37,4 +36,14 @@ resource "aws_instance" "notely_ec2" {
   tags = {
     Name = "NotelyApp"
 }
+}
+
+# Allocate and associate an Elastic IP so the instance always has a public IP
+resource "aws_eip" "notely_eip" {
+  instance = aws_instance.notely_ec2.id
+}
+
+# Output the public IP for GitHub Actions
+output "ec2_public_ip" {
+  value = aws_eip.notely_eip.public_ip
 }
