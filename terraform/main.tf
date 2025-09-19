5ddf1b9a316aa911c0d502d57eb8bbaffe412dfa
SenorGunter <163559186+SenorGunter@users.noreply.github.com>
2025-09-19 15:51:19 +0200

    trying fix4

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,9 @@ resource "aws_instance" "notely_ec2" {
   instance_type = var.ec2_instance_type
   key_name = var.ec2_key_name
   vpc_security_group_ids = [var.security_group_id]
+  subnet_id = aws_subnet.public_subnet.id
+
+  associate_public_ip_address = true
 
   user_data = <<-EOF
               #!/bin/bash
